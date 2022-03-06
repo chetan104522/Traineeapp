d5a563e1a6e8248d12942eec0d227daa24292b37
chetan <chetan.codenote@gmail.com>
2022-03-06 15:03:54 +0530

    add flash message

--- app/models/main_controller.rb
+++ app/models/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
-    def index
-    end
+  def index
+    flash[:notice] = "Logged in scessfully"
+    flash[:alert] = "Invalid user name and password" 
+  end
 end
